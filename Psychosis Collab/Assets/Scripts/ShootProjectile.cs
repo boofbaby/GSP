a0ded10b5e7b6ee6ce8d56cc9a52f7a9490566a9
Patrick Stanton <patrickstanton50@gmail.com>
2020-02-12 15:51:24 +0000

    Added recoil + level gen merge

--- Psychosis Collab/Assets/Scripts/ShootProjectile.cs
+++ Psychosis Collab/Assets/Scripts/ShootProjectile.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class ShootProjectile : MonoBehaviour
 {
-
+    public GameObject gun;
     public Rigidbody projectile;
     public float speed = 20f;
     public float size = 1f;
     public float firerate = 10f;
     float firetimer;
     public GameObject GunBarrel;
+    public Transform recoilBeginPos;
+    public Transform recoilEndPos;
+    public float recoilSpeed;
+    public Vector3 currentOffset;
+    public Vector3 currentAngleOffset;
+
+    [SerializeField]
+    private Vector3 gunOrigin;
+
+    [SerializeField]
+    private Vector3 gunAngleOrigin;
+
+    private void Start()
+    {
+        gunOrigin = gun.transform.localPosition;
+        gunAngleOrigin = gun.transform.localEulerAngles;
+        currentAngleOffset = Vector3.zero;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetButton("Fire1"))
         {
             if (Time.time - firetimer > 1 / firerate)
@@ -26,12 +44,31 @@ public class ShootProjectile : MonoBehaviour
                 instantiatedProjectile.velocity = Camera.main.transform.forward * speed;
                 instantiatedProjectile.transform.localScale = new Vector3(size, size, size);
 
+                Recoil();
+
                 Physics.IgnoreCollision(instantiatedProjectile.GetComponent<SphereCollider>(), GetComponent<CapsuleCollider>());
                 Physics.IgnoreCollision(instantiatedProjectile.GetComponent<SphereCollider>(), GunBarrel.GetComponentInParent<BoxCollider>());
             }
 
         }
 
+        gun.transform.localPosition = gunOrigin + currentOffset;
+        gun.transform.localEulerAngles = gunAngleOrigin + currentAngleOffset;
     }
-}
 
+    private void FixedUpdate()
+    {
+        currentOffset = Vector3.Lerp(currentOffset, Vector3.zero, 0.1f);
+        currentAngleOffset = Vector3.Lerp(currentAngleOffset, Vector3.zero, 0.1f);
+    }
+
+
+    public void Recoil()
+    {
+        currentOffset += Vector3.back * recoilSpeed;
+        currentOffset += Vector3.up * recoilSpeed / 3;
+        currentAngleOffset += Vector3.left * recoilSpeed;
+    }
+
+
+}
